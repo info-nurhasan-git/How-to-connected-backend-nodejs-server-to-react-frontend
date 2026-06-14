d3c82fba20d71b1a90f100977747ea86e39a6922
Nur Hasan <info.official.nurhasan.live@gmail.com>
2026-06-14 12:37:27 +0530

    backend

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/api/message", (req, res) => {
+  res.json({
+    message: "Hello from Backend!"
+  });
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
