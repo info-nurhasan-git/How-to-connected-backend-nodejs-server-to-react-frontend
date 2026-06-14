74d811c69efcb60f42554beb857ae09c58831675
Nur Hasan <info.official.nurhasan.live@gmail.com>
2026-06-14 12:39:09 +0530

    Create reactCode in App.jsx

---
+++ reactCode in App.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/message")
+      .then((response) => {
+        setMessage(response.data.message);
+      });
+  }, []);
+
+  return <h1>{message}</h1>;
+}
+
+export default App;
